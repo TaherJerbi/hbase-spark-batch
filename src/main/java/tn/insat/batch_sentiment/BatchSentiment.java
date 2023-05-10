7378ff53628cc8d8c0bf36a83b73eae5ffcbac0a
Taher Jerbi <jerbi.taherr@gmail.com>
2023-05-10 22:36:28 +0100

    feat: send to kafka

--- src/main/java/tn/insat/batch_sentiment/BatchSentiment.java
+++ src/main/java/tn/insat/batch_sentiment/BatchSentiment.java
@@ -9,9 +9,40 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
-
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 public class BatchSentiment {
 
+    public Producer makeProducer() {
+        // Creer une instance de proprietes pour acceder aux configurations du producteur
+        Properties props = new Properties();
+
+        // Assigner l'identifiant du serveur kafka
+        props.put("bootstrap.servers", "kafka:9092");
+
+        // Definir un acquittement pour les requetes du producteur
+        props.put("acks", "all");
+
+        // Si la requete echoue, le producteur peut reessayer automatiquemt
+        props.put("retries", 0);
+
+        // Specifier la taille du buffer size dans la config
+        props.put("batch.size", 16384);
+
+        // buffer.memory controle le montant total de memoire disponible au producteur pour le buffering
+        props.put("buffer.memory", 33554432);
+
+        props.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+
+        props.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+
+
+        return new KafkaProducer<String, String>(props);
+    }
     public void countSentiments() {
         Configuration config = HBaseConfiguration.create();
         SparkConf sparkConf = new SparkConf().setAppName("SparkHBaseTest").setMaster("local[4]");
@@ -39,6 +70,11 @@ public class BatchSentiment {
         // save the results to HBase
         HbaseRepository hbaseRepository = HbaseRepository.getInstance();
         hbaseRepository.insertResult(sentimentCounts.collect());
+
+        // send the results to Kafka
+        Producer<String, String> producer = makeProducer();
+        String message = sentimentCounts.collect().toString();
+        producer.send(new ProducerRecord<String, String>("batch_result", message));
     }
 
     public static void main(String[] args){
